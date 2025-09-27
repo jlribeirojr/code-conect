343e5f6ee39bb4af082ba677be4c30a25ff3c9b1
jlribeirojr <jlribeirojr@gmail.com>
2025-09-27 01:15:10 -0300

    implementação dos arquivos    modified:   combined.log         modified:   package-lock.json         modified:   package.json         modified:   src/components/CardPost/cardpost.module.css         modified:   src/components/CardPost/index.jsx e src/app/posts

---
+++ src/app/posts/[slug]/page.js
@@ -0,0 +1,33 @@
+import logger from "@/logger";
+import { remark } from "remark";
+import html from "remark-html";
+
+async function getPostBySlug(slug) {
+  const url = `http://localhost:3042/posts?slug=${slug}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    logger.error("algo deu muito errado");
+    return {};
+  }
+  logger.info("Posts obtidos com sucesso!");
+  const data = await response.json();
+  if (data.length == 0) {
+    return {};
+  }
+  const post = data[0];
+  const processedContent = await remark()
+    .use(html)
+    .process(post.markdown);
+  const contentHtml = processedContent.toString();
+  post.markdown = contentHtml
+  return post;
+}
+const PagePost = async ({ params }) => {
+  const post = await getPostBySlug(params.slug);
+  return <>
+  <h1 style={{ color: "white" }}>{post.title}</h1>
+  <div style={{ padding: 16, background: 'black'}}dangerouslySetInnerHTML={{ __html: post.markdown }} />
+
+  </>
+};
+export default PagePost;
--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,29 +1,33 @@
-import Image from "next/image"
-import { Avatar } from "../Avatar"
-import Styles from './cardpost.module.css'
+import Image from "next/image";
+import { Avatar } from "../Avatar";
+import Styles from "./cardpost.module.css";
+import Link from "next/link";
 
-export const CardPost = ({post}) =>{
-    return(
-        <div>
+export const CardPost = ({ post }) => {
+  return (
+    <Link href={`/posts/${post.slug}`} className={Styles.Link}>
+      <div>
         <article className={Styles.card}>
-            <header className={Styles.header}>
-                <figure>
-                    <Image src={post.cover} width={438} height={133} alt={`Capa do post do titulo: ${post.title}`}/>
-                </figure>
-            </header>
-            <section>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-                texto
-            </section>
-            <footer className={Styles.footer}>
-                <Avatar
-                imageSrc={post.author.avatar}
-                name={post.author.username}
-                />
-            </footer>
+          <header className={Styles.header}>
+            <figure>
+              <Image
+                src={post.cover}
+                width={438}
+                height={133}
+                alt={`Capa do post do titulo: ${post.title}`}
+              />
+            </figure>
+          </header>
+          <section>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            texto
+          </section>
+          <footer className={Styles.footer}>
+            <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+          </footer>
         </article>
-
-        </div>
-    )
-}
+      </div>
+    </Link>
+  );
+};
